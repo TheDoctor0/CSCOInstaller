2acb4d3e4d20c6453f6faea22b2e1e4ef4387ee0
OZone998 <ellie2213@gmail.com>
2016-12-30 13:00:39 +0100

    v1.0
    Initial release.

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CSCOUpdater
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.installed_v = new System.Windows.Forms.Label();
+            this.latest_v = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(15, 112);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // installed_v
+            // 
+            this.installed_v.AutoSize = true;
+            this.installed_v.Location = new System.Drawing.Point(42, 16);
+            this.installed_v.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.installed_v.Name = "installed_v";
+            this.installed_v.Size = new System.Drawing.Size(49, 13);
+            this.installed_v.TabIndex = 1;
+            this.installed_v.Text = "Installed:";
+            // 
+            // latest_v
+            // 
+            this.latest_v.AutoSize = true;
+            this.latest_v.Location = new System.Drawing.Point(149, 16);
+            this.latest_v.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.latest_v.Name = "latest_v";
+            this.latest_v.Size = new System.Drawing.Size(39, 13);
+            this.latest_v.TabIndex = 2;
+            this.latest_v.Text = "Latest:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 167);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(239, 20);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(100, 196);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CS:CO Installer v1.0 by O\'Zone";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "C:\\Program Files (x86)\\Steam";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Steam Folder:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.latest_v);
+            this.groupBox1.Controls.Add(this.installed_v);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(241, 34);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Version";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(185, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 218);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "CS:CO Installer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label installed_v;
+		private System.Windows.Forms.Label latest_v;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+
+using System.Reflection;
+
+using System.Web;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+
+namespace CSCOUpdater
+{
+    public partial class Form1 : Form
+    {
+        string v_url = "http://cs-reload.pl/csco/";
+
+        double latest_version = 0.0;
+        double installed_version = 0.0;
+
+        bool done = false;
+        bool downloading = false;
+
+        WebClient client;
+
+        DialogResult r;
+
+        string latest_url = "";
+        string csco_path = "";
+
+        public Form1()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckVersion();
+        }
+
+        private void CheckVersion()
+        {
+            this.ActiveControl = label1;
+
+            latest_version = 0.0;
+            installed_version = 0.0;
+
+            string v_file = "NULL";
+            string local_v_file = "NULL";
+
+            button1.Enabled = false;
+
+            csco_path = textBox1.Text + "/steamapps/sourcemods";
+
+            try
+            {
+                using (System.Net.WebClient client = new System.Net.WebClient())
+                {
+                    v_file = client.DownloadString(v_url);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error! Unable to get version file.\nCheck your internet connection!");
+            }
+
+            if (v_file != "NULL")
+            {
+                latest_version = 0.0;
+
+                string buff = "";
+
+                foreach (char c in v_file)
+                {
+                    if (c == ' ')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        buff += c;
+                    }
+                }
+
+                latest_version = Convert.ToDouble(buff);
+
+                buff = "";
+
+                bool reading = false;
+
+                foreach (char c in v_file)
+                {
+                    if (!reading)
+                    {
+                        if (c == ' ')
+                        {
+                            reading = true;
+                        }
+                    }
+                    else
+                    {
+                        buff += c;
+                    }
+                }
+
+                latest_url = buff;
+            }
+
+            if (Directory.Exists(csco_path + "/csco"))
+            {
+
+                try
+                {
+                    local_v_file = File.ReadAllText(csco_path + "/csco/version.txt");
+                    installed_version = Convert.ToDouble(local_v_file);
+                }
+                catch (Exception)
+                {
+
+                    r = MessageBox.Show("File version.txt not found. Do you have the latest version?",
+                        "Error", MessageBoxButtons.YesNo);
+
+                    if (r == DialogResult.Yes)
+                    {
+                        File.WriteAllText(csco_path + "/csco/version.txt", "" + latest_version);
+
+                        installed_version = latest_version;
+                    }
+                    else
+                    {
+                        label3.Text = "Unknown";
+                        installed_version = -0.1;
+                    }
+                }
+            }
+
+            if (installed_version > 0.0)
+            {
+                label3.Text = Convert.ToString(installed_version);
+            } 
+            else if (installed_version == 0.0)
+            {
+                label3.Text = "None";
+            }
+
+            label4.Text = Convert.ToString(latest_version);
+
+            if (latest_url != "")
+            {
+                if (!Directory.Exists(csco_path + "/csco"))
+                {
+                    button1.Text = "Download";
+                    button1.Enabled = true;
+                }
+                else
+                {
+                    if (installed_version < latest_version)
+                    {
+                        button1.Text = "Update";
+                        button1.Enabled = true;
+                    }
+                    else
+                    {
+                        button1.Text = "No Update Required";
+                        button1.Enabled = false;
+                    }
+                }
+            }
+            else
+            {
+                button1.Enabled = false;
+                button1.Text = "Update Not Possible!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBox1.Text + "/Steam.exe"))
+            {
+                csco_path = textBox1.Text + "/steamapps/sourcemods";
+
+                IProgress<double> progress = new Progress<double>(b => progressBar1.Value = (int)b);
+
+                if (!done && button1.Enabled)
+                {
+                    button1.Text = "Downloading...";
+
+                    button1.Enabled = false;
+
+                    textBox1.Enabled = false;
+
+                    downloading = true;
+
+                    delete_dir(csco_path + "/Temp");
+
+                    Directory.CreateDirectory(csco_path + "/Temp");
+
+                    try
+                    {
+                        client = new WebClient();
+                        client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                        client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                        client.DownloadFileAsync(new Uri(latest_url), csco_path + "/Temp/csco.zip");
+                    }
+                    catch (Exception ee)
+                    {
+                        MessageBox.Show("Error: " + ee.Message);
+                        done = true;
+                    }
+                }
+                else
+                {
+                    button1.Text = "Update In Progress!";
+                    button1.Enabled = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error: Check path to your Steam directory!");
+            }
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+
+            progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+
+        private void client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                delete_dir(csco_path + "/Temp");
+            }
+            else
+            {
+
+                try
+                {
+                    if (!done)
+                    {
+                        button1.Text = "Installing...";
+
+                        done = true;
+
+                        progressBar1.Maximum = 100;
+
+                        progressBar1.Value = 100;
+
+                        delete_dir(csco_path + "/csco");
+
+                        try
+                        {
+                            ZipFile.ExtractToDirectory(csco_path + "/Temp/csco.zip", csco_path + "/");
+                        }
+                        catch (Exception ee)
+                        {
+                            MessageBox.Show("Error: " + ee.Message);
+                        }
+
+                        if (File.Exists(csco_path + "/HostMe.txt"))
+                        {
+                            File.Delete(csco_path + "/HostMe.txt");
+                        }
+
+                        if (File.Exists(csco_path + "/ReadMe.txt"))
+                        {
+                            File.Delete(csco_path + "/ReadMe.txt");
+                        }
+
+                        if (!File.Exists(csco_path + "/csco/version.txt"))
+                        {
+                            File.WriteAllText(csco_path + "/csco/version.txt", "" + latest_version);
+                        }
+
+                        delete_dir(csco_path + "/Temp");
+
+                        foreach (var process in Process.GetProcessesByName("Steam"))
+                        {
+                            process.Kill();
+                        }
+
+                        label4.Text = Convert.ToString(latest_version);
+
+                        button1.Text = "Installation Complete";
+
+                        downloading = false;
+                    }
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show(ee.Message);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CheckVersion();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (downloading)
+            {
+                var res = MessageBox.Show(this, "Downloading in progress. You want to quit?", "Exit",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+
+                if (res == DialogResult.Yes)
+                {
+                    client.CancelAsync();
+                }
+            }
+        }
+
+        public void delete_dir(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                if (path.Contains("csco"))
+                {
+                    string[] fileCollection = Directory.GetFiles(path);
+
+                    foreach (String file in fileCollection)
+                    {
+                        File.SetAttributes(file, FileAttributes.Normal);
+                    }
+                }
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+                Directory.Delete(path);
+            }
+        }
+    }
+}
